c2aa4a1b0d4216b192f0b0507e026ce20352c9d3
timothy-20 <jungun3329@gmail.com>
2023-01-12 22:47:46 +0900

    230112) - selenium 의존성 제거 및 crawler 소스 전면 수정.

--- src/main/java/com/timothy20/TKWebCrawler/controller/TKWebCrawlerController.java
+++ src/main/java/com/timothy20/TKWebCrawler/controller/TKWebCrawlerController.java
@@ -1,13 +1,7 @@
 package com.timothy20.TKWebCrawler.controller;
 
-import com.timothy20.TKWebCrawler.TKWebCrawlerApplication;
 import org.apache.logging.log4j.LogManager;
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -20,19 +14,8 @@ import java.util.logging.Logger;
 
 @Controller
 public class TKWebCrawlerController {
-    final Logger logger = (Logger) LogManager.getLogger(TKWebCrawlerController.class);
-    final WebDriver webDriver;
-
     public TKWebCrawlerController() {
-        System.setProperty("webdriver.chrome.driver", "./webdriver/chromedriver");
 
-        ChromeOptions options = new ChromeOptions();
-
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        options.setImplicitWaitTimeout(Duration.ofSeconds(2));
-        options.setHeadless(true);
-
-        this.webDriver = new ChromeDriver(options);
     }
 
     @PostMapping(value = "/receive_target_url")
@@ -40,7 +23,6 @@ public class TKWebCrawlerController {
         ResponseEntity<Map<String, Object>> response;
 
         try {
-            this.webDriver.get(targetURL);
             response = new ResponseEntity<>(HttpStatus.OK);
 
         } catch (Exception exception) {
@@ -51,7 +33,6 @@ public class TKWebCrawlerController {
             response = new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
 
         } finally {
-            this.webDriver.quit();
         }
 
         return response;
