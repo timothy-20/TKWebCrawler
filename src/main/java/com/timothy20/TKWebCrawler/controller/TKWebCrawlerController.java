954366624ad48b17f36d87b9880bce9ea3694638
timothy-20 <jungun3329@gmail.com>
2023-01-12 23:55:45 +0900

    230112) - additional

--- src/main/java/com/timothy20/TKWebCrawler/controller/TKWebCrawlerController.java
+++ src/main/java/com/timothy20/TKWebCrawler/controller/TKWebCrawlerController.java
@@ -1,11 +1,15 @@
 package com.timothy20.TKWebCrawler.controller;
 
+import jakarta.validation.constraints.NotNull;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -38,4 +42,15 @@ public class TKWebCrawlerController {
         return response;
     }
 
+    private void makeConnectionWithURLString(@NotNull String urlString)
+    {
+        URL url = new URL(urlString);
+        URLConnection urlConnection = url.openConnection();
+//        HttpURLConnection connection =
+    }
+
+//    private StringBuffer readHTMLFromURL(String url)
+//    {
+//
+//    }
 }
